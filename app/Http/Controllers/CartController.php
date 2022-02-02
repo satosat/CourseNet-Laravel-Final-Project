36f25fdeedf953b67c3c823409debb66b7426a42
satosat <satriotimotius@gmail.com>
2022-02-02 19:23:40 +0700

    added add to cart functionality

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -19,6 +19,33 @@ class CartController extends Controller
 
         return view('cart.index', [
             'title' => 'cart - readme',
+            'carts' => $carts,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'book_id' => 'required'
+        ]);
+
+        $row = Cart::where([
+            'user_id' => Auth::id(),
+            'book_id' => $request->book_id,
+        ]);
+
+        if(count($row->get()))
+        {
+            $row->delete();
+        }
+        else
+        {
+            Cart::create([
+                'user_id' => Auth::id(),
+                'book_id' => $request->book_id,
+            ]);
+        }
+
+        return response()->noContent();
     }
 }
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,6 +2,46 @@
 
 @section('content')
 
+    @if (!count($carts))
+        <div class="row text-center">
+            <h2 class="display-6">You don't have anything on your cart yet</h2>
+        </div>
+    @endif
 
+    @foreach ($carts as $cart)
+        <div class="row g-2">
+            <div class="col-6 p-2">
+                <div class="p-3 border bg-light card">
+                    <a href="/book/{{ $cart->book_id }}">
+                        {{ $cart->book->title }} <br>
+                        {{ $cart->book->author }}
+                    </a>
+                </div>
+                <div class="d-flex justify-content-evenly">
+                    <button class="btn btn-primary">Rent Book</button>
+                    <button class="btn btn-success">Buy Book</button>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    <style>
+        .card {
+            display: flex;
+            height: 100px;
+            justify-content: space-around;
+            align-items: center;
+            text-align: center;
+        }
+
+        .card a {
+            text-decoration: none;
+            color: black;
+        }
+
+        .card a:hover {
+            text-decoration: underline;
+        }
+    </style>
 
 @endsection
